eedd7110264d2dd9b0f8a316c1ba4fa8d5f3ecf3
robertbadas <robertbadas@gmail.com>
2017-04-05 14:18:46 +0200

    Update trim.js

--- trim.js
+++ trim.js
@@ -1,7 +1,51 @@
 function trim(string, char){
-    if(!char) char = ' '; //space by default
-    char = char.replace(/([()[{*+.$^\\|?])/g, '\\$1'); //escape char parameter if needed for regex syntax.
-    var regex_1 = new RegExp("^" + char + "+", "g");
-    var regex_2 = new RegExp(char + "+$", "g");
-    return string.replace(regex_1, '').replace(regex_2, '');
+  	var whitespace = [
+		' ',
+		'\n',
+		'\r',
+		'\t',
+		'\f',
+		'\x0b',
+		'\xa0',
+		'\u2000',
+		'\u2001',
+		'\u2002',
+		'\u2003',
+		'\u2004',
+		'\u2005',
+		'\u2006',
+		'\u2007',
+		'\u2008',
+		'\u2009',
+		'\u200a',
+		'\u200b',
+		'\u2028',
+		'\u2029',
+		'\u3000'
+	].join('')
+	var l = 0
+	var i = 0
+	str += ''
+
+	if (charlist) {
+		whitespace = (charlist + '').replace(/([[\]().?/*{}+$^:])/g, '$1')
+	}
+
+	l = str.length
+	for (i = 0; i < l; i++) {
+		if (whitespace.indexOf(str.charAt(i)) === -1) {
+			str = str.substring(i)
+			break
+		}
+	}
+
+	l = str.length
+	for (i = l - 1; i >= 0; i--) {
+		if (whitespace.indexOf(str.charAt(i)) === -1) {
+			str = str.substring(0, i + 1)
+			break
+		}
+	}
+
+	return whitespace.indexOf(str.charAt(0)) === -1 ? str : ''
 }
